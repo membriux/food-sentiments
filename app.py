d8e729d6445283ff8464d5afa87fc58c00fcf5df
Guillermo Ignacio Sanchez Lamas <sanguillermo98@gmail.com>
2019-02-28 16:01:04 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from Analyzer import get_businesses
 
+
+BUSINESSES = []
 
 app = Flask(__name__)
 
@@ -9,7 +12,22 @@ def index():
     return 'Welcome!'
 
 
+# Get the info about business
+def analyze_businesses():
+    global BUSINESSES
+    BUSINESSES = get_businesses()
+    print('Analyzing businesses\n')
+    for b in BUSINESSES:
+        print(b.name)
+        print(b.review_count)
+        print(b.stars)
+        print(b.reviews)
+        print(b.overall_sentiment)
+
+
 if __name__ == '__main__':
+    print('Running app...')
+    analyze_businesses()
     app.run()
 
 
